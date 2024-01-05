ec189127396791ef152fe9263ce0817723a6bebd
WrothSquid37 <42189580+WrothSquid37@users.noreply.github.com>
2024-01-05 14:10:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var email1 = document.getElementById('email1');
 var email2 = document.getElementById('email2');
 
+console.log(email1.name);
+console.log(email2.name);
+
 email1.style.display = 'none';
 email2.style.display = 'none';
