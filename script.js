26ecdf5a3c24756e80bdd65e4cac925bc1cfc889
WrothSquid37 <42189580+WrothSquid37@users.noreply.github.com>
2024-01-05 14:23:59 -0800

    Update script.js with hide funcitons

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 var email1 = document.getElementById('email1');
 var email2 = document.getElementById('email2');
-
-console.log(email1.name);
-console.log(email2.name);
+var email3 = document.getElementById('email1');
+var email4 = document.getElementById('email2');
 
 email1.style.display = 'none';
 email2.style.display = 'none';
+email3.style.display = 'none';
+email4.style.display = 'none';
+
+function displayEmail(int index) {
+    hideAllEmails();
+    switch(index) {
+      case 0:
+        email1.style.display = 'block';
+      break;
+      case 1:
+        email2.style.display = 'block';
+      break;
+      case 2:
+        email3.style.display = 'block';
+      break;
+      case 3:
+        email4.style.display = 'block';
+      break;
+      default:
+        console.log("Error invalid index");
+    }
+}
+
+function hideAllEmails() {
+    email1.style.display = 'none';
+    email2.style.display = 'none';
+    email3.style.display = 'none';
+    email4.style.display = 'none';
+}
